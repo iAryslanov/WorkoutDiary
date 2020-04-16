1494b42ca3483392c08ba65da1b5aabcb55881e5
iAryslanov <Aryslanov@gmail.com>
2020-04-16 22:41:17 +0300

    add project files

---
+++ WorkoutDiary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorkoutDiary
+//
+//  Created by Igor Aryslanov on 15.04.2020.
+//  Copyright © 2020 Igor Aryslanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
