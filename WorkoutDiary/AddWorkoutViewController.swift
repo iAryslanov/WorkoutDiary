a3a83771ddab57a291482fae5574c8789925b4f2
iAryslanov <Aryslanov@gmail.com>
2020-04-17 10:59:43 +0300

    update (AppDelegate): add rootViewController

---
+++ WorkoutDiary/AddWorkoutViewController.swift
@@ -0,0 +1,9 @@
+//
+//  AddWorkoutViewController.swift
+//  WorkoutDiary
+//
+//  Created by Igor Aryslanov on 17.04.2020.
+//  Copyright © 2020 Igor Aryslanov. All rights reserved.
+//
+
+import Foundation
